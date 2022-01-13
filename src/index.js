bd26980b223d3db186d9497fe9494667ab1a7823
masood <masooodm@yahoo.com>
2022-01-13 18:51:05 +0330

    some minor changes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import "./todo-mvc-app.css";
-import "./todo-mvc-base.css";
 
 ReactDOM.render(
   <React.StrictMode>
